1e3a2e862a60b5bd5322c4a9272347d1e453afc3
zhaoye <yue.joo@gmail.com>
2019-09-02 11:51:16 +0800

    Add pre-build jar

--- ProdLinearity/bin/prodlinearity.sh
+++ ProdLinearity/bin/prodlinearity.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
 DIR="$( cd "$( dirname "$BASEDIR" )" >/dev/null 2>&1 && pwd )"
+
+# Use the prebuild jar
+# java -jar ${DIR}/pre-build-jar/ProdLinearity-1.0-SNAPSHOT.jar $@
 java -jar ${DIR}/target/ProdLinearity-1.0-SNAPSHOT.jar $@
 
